ecf440883e237d5f86e329a749b76077a85e8ec5
vitvitcher <79949751+vitvitcher@users.noreply.github.com>
2026-04-05 17:20:44 +0200

    very minor change

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import css from './SearchBox.module.css'
-import { useDebouncedCallback } from 'use-debounce';
+// import { useDebouncedCallback } from 'use-debounce';
 
 
 interface SearchBoxProps {
